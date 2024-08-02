528fb103f904ea4d7248804d14d3b7b1ad68cc5a
Mips2648 <mips2648@gmail.com>
2024-08-02 17:22:10 +0200

    list files

--- .github/workflows/translate.py
+++ .github/workflows/translate.py
@@ -1,4 +1,31 @@
 import os
 
-github_workspace = os.environ.get("GITHUB_WORKSPACE")
-print(github_workspace)
+# github_workspace = os.environ.get("GITHUB_WORKSPACE")
+
+def get_textes_from_source():
+    print("Recherche de textes dans le code...")
+    for root, dirs, files in os.walk(os.environ.get("GITHUB_WORKSPACE")):
+        for dirname in dirs:
+            if dirname[0] == ".":
+                dirs.remove(dirname)
+            if (root.endswith("/core") and dirname == 'i18n'):
+                dirs.remove(dirname)
+            # for d in excludes:
+            #     if fnmatch.fnmatch(dirname, d):
+            #         dirs.remove(dirname)
+
+        for fileName in files:
+            # for f in excludes:
+            #     if fnmatch.fnmatch(fileName, f):
+            #         continue
+            if ( fileName[-4:] == ".php"
+              or fileName[-3:] == ".js"
+              or fileName[-5:] == ".json"
+              or fileName[-5:] == ".html"):
+                absolute_path = f"{root}/{fileName}"
+                print(f"    {absolute_path}...")
+                # fichier = FichierSource(f"{absolute_path}")
+                # fichier.search_textes()
+    return
+
+get_textes_from_source()
